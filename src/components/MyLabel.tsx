370a10b64a0311c99c9c47731e7b608dc8570e00
Brayan1951 <brayanc1951@gmail.com>
2022-06-16 22:43:41 -0400

    primer commit

---
+++ src/components/MyLabel.tsx
@@ -0,0 +1,25 @@
+import './mylabel.css'
+
+
+export interface Props {
+  /**
+    * this is the message of the label
+    */
+  label?: string
+  /**
+   * this is the options of the label: 'normal' , 'h1' , 'h2' , 'h3'
+   */
+  size?: 'normal' | 'h1' | 'h2' | 'h3'
+  allCaps?: boolean
+  color?: 'primary' | 'secondary' | 'tertiary'
+  fontColor?: string
+
+}
+
+
+
+
+export const MyLabel = ({ label = 'No label', size = 'normal', allCaps = false, color = 'primary', fontColor = '#5517ac' }: Props) => {
+  return (
+    <span style={{ color: fontColor }} className={`label ${size} text-${color}`}>{allCaps ? label.toUpperCase() : label}</span>)
+}
